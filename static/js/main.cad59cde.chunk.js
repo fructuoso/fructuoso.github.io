69cbdc0effec274be4b0c77fffe41325ce1fac7f
Fructuoso <victor@fructuoso.com.br>
2020-11-11 13:56:49 -0300

    Updates

---
+++ static/js/main.cad59cde.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfructuoso.github.io"]=this["webpackJsonpfructuoso.github.io"]||[]).push([[0],{33:function(e,t,c){"use strict";c.r(t);var o=c(4),r=(c(1),c(17)),s=c.n(r),u=c(50),n=c(49),i=c(51);function a(){return Object(o.jsx)(i.a,{children:"Hello World REACT!"})}var b=c(22),j=Object(b.a)({});s.a.render(Object(o.jsxs)(n.a,{theme:j,children:[Object(o.jsx)(u.a,{}),Object(o.jsx)(a,{})]}),document.querySelector("#root"))}},[[33,1,2]]]);
+//# sourceMappingURL=main.cad59cde.chunk.js.map
